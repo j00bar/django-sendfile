e823026e46d0c0c38771d43422b9480e05e7b5b8
Joshua "jag" Ginsberg <jag@flowtheory.net>
2015-06-10 12:35:42 -0400

    Allow xsendfile relative to SENDFILE_ROOT

--- sendfile/backends/xsendfile.py
+++ sendfile/backends/xsendfile.py
@@ -1,8 +1,16 @@
+import os
+
+from django.conf import settings
 from django.http import HttpResponse
 
 
 def sendfile(request, filename, **kwargs):
     response = HttpResponse()
+    # If SENDFILE_ROOT is defined, the filename to use should be relative to 
+    # SENDFILE_ROOT instead of MEDIA_ROOT
+    if getattr(settings, 'SENDFILE_ROOT', None):
+        filename = os.path.join(settings.SENDFILE_ROOT,
+                                os.path.relpath(filename, settings.MEDIA_ROOT))
     response['X-Sendfile'] = unicode(filename).encode('utf-8')
 
     return response
